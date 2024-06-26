7f229774b74afda14b96f2321afe0c5122ef770d
headrick leonard <headricleonard@gmail.com>
2024-06-26 16:52:12 +0300

    modified the form and the team section

--- src/app/components/ProfileCard.jsx
+++ src/app/components/ProfileCard.jsx
@@ -79,7 +79,7 @@ import React from "react";
 
 const ProfileCard = ({ name, title, description, image }) => {
   return (
-    <div className="w-[30rem] mx-auto flex flex-col gap-2 my-12 px-4 border border-gray-300/20 rounded-lg backdrop-blur-md dark:bg-gray-900 shadow-lg">
+    <div className="md:w-[25rem] xl:w-[27rem] 2xl:w-[30rem] mx-auto flex flex-col gap-2 my-12 px-4 border border-gray-300/20 rounded-lg backdrop-blur-md dark:bg-gray-900 shadow-lg">
       <div className="w-full flex justify-center items-center">
         <img
           className="w-[8rem] h-[8rem] rounded-full outline outline-offset-2 outline-1 outline-blue-400 shadow-lg relative -top-[4rem]"
